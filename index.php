3ea14dcf890ceb7f4c3a036d1ca56cc80b519a35
Eero Otsus <eerootsus@gmail.com>
2021-11-16 12:31:47 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h1>Tere tulemast!</h1>
+        <form action="/login.php" method="post">
+            <input type="text" name="username" placeholder="Kasutaja"><br>
+            <input type="password" name="password" placeholder="Parool"><br>
+            <button>Logi sisse</button>
+        </form>
+    </body>
+</html>
