394b180848d6a9ae5d110d130a0efd8c2722c7a4
Eero Otsus <eerootsus@gmail.com>
2021-11-23 14:16:25 +0200

    Simple file storage for users

--- login.php
+++ login.php
@@ -1,2 +1,41 @@
 <?php
-var_dump($_POST);
+
+if (empty($_POST)) {
+    header('Location: /');
+    exit();
+}
+
+$users = [];
+
+$handle = fopen("users.csv", "r");
+$row_count = 0;
+while (($user = fgetcsv($handle)) !== FALSE) {
+    $row_count++;
+
+    if($row_count === 1) {
+        continue;
+    }
+
+    $users[] = [
+        'username' => $user[0],
+        'password' => $user[1],
+    ];
+}
+
+$logged_in_user = false;
+foreach($users as $user) {
+
+    if ($_POST['username'] === $user['username'] && $_POST['password'] === $user['password']){
+        $logged_in_user = $user;
+        break;
+    }
+
+}
+
+if (empty($logged_in_user)) {
+    throw new Error('Unauthorized');
+}
+
+echo 'Hello '.$logged_in_user['username'];
+
+
